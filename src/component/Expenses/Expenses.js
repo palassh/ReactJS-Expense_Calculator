07e88e916726ba02b7b4a29183c68acc680e5b04
Sahu <palash.sahu@capgemini.com>
2022-05-20 13:40:54 +0530

    init

---
+++ src/component/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Expense.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpenseChart";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpense = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseChart expenses={filteredExpense}/>
+        <ExpensesList items={filteredExpense} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
